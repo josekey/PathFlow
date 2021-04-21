7d23a5f8de4b17afc8f961c87413f636c673da8b
Jose Key <jose.key@yale.edu>
2021-04-21 01:20:06 -0400

    First commit!

---
+++ app.py
@@ -0,0 +1,62 @@
+# libraries to work with data
+
+# web app libraries
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+
+# error handeling
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+
+# Configure application
+app = Flask(__name__)
+app.secret_key='hello'
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+### beginning of app routes ### 
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method =="POST":
+        return 'TODO'
+    else:
+        return render_template("home.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method =="POST":
+        return 'TODO'
+    else:
+        return render_template("login.html")
+
+@app.route("/entry", methods=["GET", "POST"])
+def entry():
+    if request.method == "POST":
+        return 'TODO'
+    else:
+        return render_template('entry.html')
+
+@app.route("/past_entry", methods=["GET", "POST"])
+def past_entry():
+    if request.method == "POST":
+        return 'TODO'
+    else:
+        return render_template('past_entry.html')
+
+# [https://flask.palletsprojects.com/en/1.1.x/errorhandling/]
+@app.errorhandler(Exception)
+def errorhandler(e):
+    """Handle error"""
+    if not isinstance(e, HTTPException):
+        e = InternalServerError()
+    return render_template('error.html', name = e.name, code = e.code)
